5a5024ea3210dc0914119eeed76f319e66e7edb4
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-04-18 18:19:49 +0300

    feat: add `flatten` function to replace part of NestedMKDict with FlatMKDict

---
+++ multikeydict/flatten.py
@@ -0,0 +1,34 @@
+from .nestedmkdict import NestedMKDict
+from .flatmkdict import FlatMKDict
+
+from typing import Sequence
+
+def _select(seq: Sequence, elems: set):
+	selected = []
+	rest = []
+	for el in seq:
+		if el in elems:
+			selected.append(el)
+		else:
+			rest.append(el)
+	return rest, selected
+
+def flatten(mkdict, selkeys: Sequence=()) -> NestedMKDict:
+	selkeys_set = set(selkeys)
+	newdict = mkdict._wrap({}, parent=mkdict)
+	newdict._parent = None
+
+	for key, v in mkdict.walkitems():
+		keys_nested, keys_flat = _select(key, selkeys_set)
+		if keys_flat:
+			flatdict = newdict.get(keys_nested, None)
+			if flatdict is None:
+				newdict[keys_nested] = (flatdict:=FlatMKDict())
+			else:
+				if not isinstance(flatdict, FlatMKDict):
+					raise KeyError(f'Unable to flatten: {".".join(key)}')
+			flatdict[keys_flat] = v
+		else:
+			newdict[key] = v
+
+	return newdict
