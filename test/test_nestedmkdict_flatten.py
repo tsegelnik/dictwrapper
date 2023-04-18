52ce027b234f690f1ee59c5d9c40eb90cb62adc6
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-04-18 18:19:56 +0300

    test: unit test for NestedMKDict

---
+++ test/test_nestedmkdict_flatten.py
@@ -0,0 +1,122 @@
+from multikeydict.nestedmkdict import NestedMKDict
+from multikeydict.flatten import flatten
+from pytest import raises
+
+from pprint import pprint
+
+def test_nestedmkdict_flatten_v01():
+    dct = {'root': {
+        'subfolder1': {
+            'key1': 'value1',
+            'key2': 'value2'
+        },
+        'subfolder2': {
+            'key1': 'value1',
+            'key2': 'value2',
+            'st': {
+                'a1': {
+                       'b1': {
+                           'c1': 'v1'
+                           }
+                       },
+                'a2': {
+                       'b2': {
+                           'c2': 'v2'
+                           }
+                       },
+                }
+        },
+        'key0': 'value0'
+    }}
+    dw = NestedMKDict(dct, recursive_to_others=True)
+    dws = NestedMKDict(dct, sep='.', recursive_to_others=True)
+
+    dwf = flatten(dw, ('a1', 'b1', 'c1', 'a2', 'b2', 'c2'))
+    dwsf = flatten(dws, ('a1', 'b1', 'c1', 'a2', 'b2', 'c2'))
+
+    for obj in (dwf, dwsf):
+        assert obj['root', 'subfolder2', 'st', 'a1', 'b1', 'c1']=='v1'
+        assert obj['root', 'subfolder2', 'st', 'b1', 'a1', 'c1']=='v1'
+        assert obj['root', 'subfolder2', 'st', 'c1', 'a1', 'b1']=='v1'
+        assert obj['root', 'subfolder2', 'st', 'a2', 'b2', 'c2']=='v2'
+        assert obj['root', 'subfolder2', 'st', 'b2', 'a2', 'c2']=='v2'
+        assert obj['root', 'subfolder2', 'st', 'c2', 'a2', 'b2']=='v2'
+
+def test_nestedmkdict_flatten_v02():
+    dct = {'root': {
+        'subfolder1': {
+            'key1': 'value1',
+            'key2': 'value2'
+        },
+        'subfolder2': {
+            'key1': 'value1',
+            'key2': 'value2',
+            'st': {
+                'a1': {
+                       'b1': {
+                           'd1': 'extra',
+                           'c1': 'v1'
+                           }
+                       },
+                'a2': {
+                       'b2': {
+                           'c2': 'v2'
+                           }
+                       },
+                }
+        },
+        'key0': 'value0'
+    }}
+    dw = NestedMKDict(dct, recursive_to_others=True)
+
+    with raises(KeyError):
+        dwf = flatten(dw, ('a1', 'b1', 'c1', 'a2', 'b2', 'c2'))
+    # import IPython; IPython.embed(colors='neutral')
+    # for obj in (dwf,):
+    #     assert obj['root', 'subfolder2', 'st', 'a1', 'b1', 'c1']=='v1'
+    #     assert obj['root', 'subfolder2', 'st', 'b1', 'a1', 'c1']=='v1'
+    #     assert obj['root', 'subfolder2', 'st', 'c1', 'a1', 'b1']=='v1'
+    #     assert obj['root', 'subfolder2', 'st', 'a2', 'b2', 'c2']=='v2'
+    #     assert obj['root', 'subfolder2', 'st', 'b2', 'a2', 'c2']=='v2'
+    #     assert obj['root', 'subfolder2', 'st', 'c2', 'a2', 'b2']=='v2'
+    #     # FlatDict is unable to pass keys
+    #     # assert obj['root', 'subfolder2', 'st', 'd1', 'a2', 'b2']=='extra'
+
+def test_nestedmkdict_flatten_v03():
+    dct = {'root': {
+        'subfolder1': {
+            'key1': 'value1',
+            'key2': 'value2'
+        },
+        'subfolder2': {
+            'key1': 'value1',
+            'key2': 'value2',
+            'st': {
+                'a1': {
+                       'b1': {
+                           'c1': 'v1'
+                           }
+                       },
+                'a2': {
+                       'b2': {
+                           'c2': 'v2',
+                           'd1': 'extra'
+                           }
+                       },
+                }
+        },
+        'key0': 'value0'
+    }}
+    dw = NestedMKDict(dct, recursive_to_others=True)
+    dwf = flatten(dw, ('a1', 'b1', 'c1', 'a2', 'b2', 'c2'))
+    pprint(dwf.object)
+    for obj in (dwf,):
+        assert obj['root', 'subfolder2', 'st', 'a1', 'b1', 'c1']=='v1'
+        assert obj['root', 'subfolder2', 'st', 'b1', 'a1', 'c1']=='v1'
+        assert obj['root', 'subfolder2', 'st', 'c1', 'a1', 'b1']=='v1'
+        assert obj['root', 'subfolder2', 'st', 'a2', 'b2', 'c2']=='v2'
+        assert obj['root', 'subfolder2', 'st', 'b2', 'a2', 'c2']=='v2'
+        assert obj['root', 'subfolder2', 'st', 'c2', 'a2', 'b2']=='v2'
+        # FlatDict is unable to pass keys
+        # assert obj['root', 'subfolder2', 'st', 'd1', 'a2', 'b2']=='extra'
+
