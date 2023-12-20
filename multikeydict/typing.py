f443882eb3865c935dd6adb13da228c83c4cf2db
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-12-20 16:26:20 +0300

    feat: add key unifiying function

--- multikeydict/typing.py
+++ multikeydict/typing.py
@@ -3,3 +3,9 @@ from typing import Tuple, Sequence, Union
 TupleKey = Tuple[str,...]
 Key = Union[str, Tuple[str,...]]
 KeyLike = Union[str, Sequence[str]]
+
+def properkey(key: KeyLike) -> Tuple[str,...]:
+    if isinstance(key, str):
+        return key,
+
+    return key
