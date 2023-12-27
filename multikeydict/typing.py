fa3ae63dfd4a1ccc1cf632405e5e922b40bcf117
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-12-27 16:28:58 +0300

    feat: multikeydict.match — unify matching iteration

--- multikeydict/typing.py
+++ multikeydict/typing.py
@@ -1,11 +1,22 @@
-from typing import Tuple, Sequence, Union
+from typing import Sequence, Tuple, Union
+from orderedset import OrderedSet
 
-TupleKey = Tuple[str,...]
-Key = Union[str, Tuple[str,...]]
+TupleKey = Tuple[str, ...]
+Key = Union[str, Tuple[str, ...]]
 KeyLike = Union[str, Sequence[str]]
 
-def properkey(key: KeyLike) -> Tuple[str,...]:
-    if isinstance(key, str):
-        return key,
 
-    return key # pyright: ignore [reportGeneralTypeIssues]
+def properkey(key: KeyLike) -> Tuple[str, ...]:
+    if isinstance(key, str):
+        return (key,)
+    if isinstance(key, Tuple):
+        return key
+
+    return tuple(key)
+
+
+def setkey(key: KeyLike) -> OrderedSet[str]:
+    if isinstance(key, str):
+        return OrderedSet((key,))
+
+    return OrderedSet(key)
---
+++ test/test_match.py
@@ -0,0 +1,38 @@
+from itertools import product
+
+from pytest import raises
+
+from multikeydict.match import match_keys
+
+
+def test_match():
+    i1 = ("1", "2", "3")
+    i2 = ("A", "B", "C")
+    i3 = ("x", "y", "z")
+
+    left = tuple((a, b) for a, b in product(i1, i2))
+    right = tuple((a,) for a, in i2)
+
+    left_bad = tuple((a, b) for a, b in product(i1, i2))
+    right_bad = tuple((a, b) for a, b in product(i1, i3))
+
+    print('left, right')
+    match_keys((left,), right, print)
+    print()
+
+    print('left+(), right')
+    match_keys((left, ((),)), right, print)
+    print()
+
+    print('left+left, right')
+    match_keys((left, left), right, print)
+    print()
+
+    print('left+right, right')
+    match_keys((left, right), right, print)
+    print()
+
+    with raises(ValueError):
+        match_keys((left_bad,), right_bad, print)
+    with raises(ValueError):
+        match_keys((left,), right_bad, print)
