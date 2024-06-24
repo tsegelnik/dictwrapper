f695a68a92042afda0493581df716b755c9cf5e5
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-06-24 12:24:02 +0300

    chore: !17

--- test/test_map.py
+++ test/test_map.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from pytest import raises
 
 from multikeydict.nestedmkdict import NestedMKDict
@@ -24,48 +26,46 @@ def test_remap_items_01():
     skip_indices_target = (("1", "z2"), ("a",))
 
     c = 0
-    for i1 in "123":
-        for i2 in "abc":
-            for i3 in "xyz":
-                d_main[i1, i2, i3] = c
-                d_reordered_match[i3, i2, i1] = c
-                if (i1 != "1" or i3 != "z") and i2 != "b":
-                    d_reordered_skipped_match[i3, i2, i1] = c
+    for i1, i2, i3 in product("123", "abc", "xyz"):
+        d_main[i1, i2, i3] = c
+        d_reordered_match[i3, i2, i1] = c
+        if (i1 != "1" or i3 != "z") and i2 != "b":
+            d_reordered_skipped_match[i3, i2, i1] = c
 
-                i2new = "A" if i2 == "a" else i2
+        i2new = "A" if i2 == "a" else i2
 
-                if i3 == "z":
-                    d_renamed_match[i1, i2new, "z1"] = c
-                    d_renamed_match[i1, i2new, "z2"] = c
+        if i3 == "z":
+            d_renamed_match[i1, i2new, "z1"] = c
+            d_renamed_match[i1, i2new, "z2"] = c
 
-                    d_renamed_reordered_match["z1", i2new, i1] = c
-                    d_renamed_reordered_match["z2", i2new, i1] = c
+            d_renamed_reordered_match["z1", i2new, i1] = c
+            d_renamed_reordered_match["z2", i2new, i1] = c
 
-                    if i2 != "b" and i1 != "1":
-                        if i2new != "a":
-                            d_renamed_reordered_skipped_both_match["z1", i2new, i1] = c
-                            d_renamed_reordered_skipped_both_match["z2", i2new, i1] = c
+            if i2 != "b" and i1 != "1":
+                if i2new != "a":
+                    d_renamed_reordered_skipped_both_match["z1", i2new, i1] = c
+                    d_renamed_reordered_skipped_both_match["z2", i2new, i1] = c
 
-                        d_renamed_reordered_skipped_source_match["z1", i2new, i1] = c
-                        d_renamed_reordered_skipped_source_match["z2", i2new, i1] = c
+                d_renamed_reordered_skipped_source_match["z1", i2new, i1] = c
+                d_renamed_reordered_skipped_source_match["z2", i2new, i1] = c
 
-                    if i2new != "a":
-                        d_renamed_reordered_skipped_target_match["z1", i2new, i1] = c
-                        if i1 != "1":
-                            d_renamed_reordered_skipped_target_match[
-                                "z2", i2new, i1
-                            ] = c
-                else:
-                    d_renamed_match[i1, i2new, i3] = c
-                    d_renamed_reordered_match[i3, i2new, i1] = c
-                    if i2new != "a":
-                        d_renamed_reordered_skipped_target_match[i3, i2new, i1] = c
-                    if i2 != "b":
-                        d_renamed_reordered_skipped_source_match[i3, i2new, i1] = c
-                        if i2new != "a":
-                            d_renamed_reordered_skipped_both_match[i3, i2new, i1] = c
+            if i2new != "a":
+                d_renamed_reordered_skipped_target_match["z1", i2new, i1] = c
+                if i1 != "1":
+                    d_renamed_reordered_skipped_target_match[
+                        "z2", i2new, i1
+                    ] = c
+        else:
+            d_renamed_match[i1, i2new, i3] = c
+            d_renamed_reordered_match[i3, i2new, i1] = c
+            if i2new != "a":
+                d_renamed_reordered_skipped_target_match[i3, i2new, i1] = c
+            if i2 != "b":
+                d_renamed_reordered_skipped_source_match[i3, i2new, i1] = c
+                if i2new != "a":
+                    d_renamed_reordered_skipped_both_match[i3, i2new, i1] = c
 
-                c += 1
+        c += 1
 
     d_new = remap_items(d_main, reorder_indices=reorder_indices)
     assert d_reordered_match == d_new
